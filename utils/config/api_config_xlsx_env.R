7ce99d72294fab1866e0721879b10f2b1856d798
vegan-cookie <sahilb@utexas.edu>
2025-04-16 17:22:46 -0700

    Extended unified config process to EAs and OPs

--- utils/config/api_config_xlsx_env.R
+++ utils/config/api_config_xlsx_env.R
@@ -52,13 +52,22 @@ token <- url_parameters[[2]]
 ### UNITS and UNIT GROUPS ### ----
 
 units <- read_excel("./utils/config/ReferenceLists/Units.xlsx", 
-                    sheet = "Updated")
+                    sheet = "Units")
 
 #we know there are duplicates in this data set
-units <- units %>% select(c(CONVERSION_FACTOR, Convertible,
-                            Sample.Unit.Group, Samples.Unit.CustomId,
-                            Samples.Unit.Name))
+units <- units %>% rename(Sample.Unit.Name = Samples.Unit.Name, 
+                          Sample.Unit.CustomId = Samples.Unit.CustomId) %>%
+            dplyr::select(c(CONVERSION_FACTOR, Convertible,
+                            Sample.Unit.Group, Sample.Unit.CustomId,
+                            Sample.Unit.Name))
 units <- distinct(units)
+
+#Sentence case in general
+#Accounting for special cases
+units <- units %>% 
+  mutate(Sample.Unit.Name = str_to_sentence(Sample.Unit.Name)) %>%
+  mutate(Sample.Unit.Name = ifelse(Sample.Unit.Name == "Ph units", 
+                                   "pH units", Sample.Unit.Name))
 
 #Elevation does not exist in the Reference Sheet
 #Needs to be added manually even though it's metres
@@ -66,8 +75,13 @@ units <- distinct(units)
 units <- units %>% add_row(CONVERSION_FACTOR = 1, 
                            Convertible = TRUE,
                            Sample.Unit.Group = "SYS-REQUIRED - Length", 
-                           Samples.Unit.CustomId = "metre",
-                           Samples.Unit.Name = "elevation")
+                           Sample.Unit.CustomId = "metre",
+                           Sample.Unit.Name = "Elevation")
+
+#fixing the units file for anomalous Count group associated with No/m2
+units <- units %>% 
+  mutate(Convertible = ifelse(Sample.Unit.Name == "Number per square meter", 
+                              FALSE, Convertible))
 
 #list of unit groups
 #sometimes this list may have one less or more unit groups than in ENV
@@ -141,7 +155,39 @@ unit_groups <- units %>%
 #   
 # }
 
+
+### EXTENDED ATTRIBUTES ### ----
+
+extendedAttributes <- read_excel("./utils/config/ReferenceLists/ExtendedAttributes.xlsx", 
+                                 sheet = "ExtendedAttributes")
+
+dropdownLists <- read_excel("./utils/config/ReferenceLists/ExtendedAttributes.xlsx", 
+                            sheet = "DropdownLists")
+
+dropdownLists <- dropdownLists %>%
+  dplyr::select(EA_customId, DDL_customId) %>%
+  group_by(EA_customId) %>%
+  summarise(
+    dropdownlist = list(
+      map(DDL_customId, ~ list(customId = .x))
+    ),
+    .groups = "drop"
+  )
+
+extendedAttributes <- extendedAttributes %>%
+  left_join(dropdownLists, by = join_by(customId == EA_customId),
+            keep = FALSE)
+
 ### OBSERVED PROPERTIES ### ----
+#need to get unit group and unit IDs prior to importing OPs
+unit_groups <- get_profiles("prod", "unitgroups") %>% 
+  rename("unit.group.id" = "id")
+
+#units without groups
+units <- get_profiles("prod", "units") %>% 
+  rename("unit.id" = "id")
+
+# EMS exported OPs --------------------------------------------------------
 OPs <- read_excel("./utils/config/ReferenceLists/Observed_Properties.xlsx")
 
 #The units don't matter for non-convertable OP units. For example microbial units. So remove them from consideration.
@@ -150,43 +196,68 @@ OPs$Sample.Unit[OPs$Convertable.In.Samples == "N"] <- ""
 #clean up modifers
 OPs$Modifier[is.na(OPs$Modifier)] <- ""
 
-#test merging the EMS parm code with unit group to make 'new codes'that can be safely used for upload
-OPs$NewCode <- paste0(OPs$Parm.Code, "-", OPs$Sample.Unit.Group, 
-                      OPs$Modifier)
+#Add an empty Result.Type column
+OPs$Result.Type <- ""
 
 #get the unique list of OP IDs
-OPs_unique <- OPs %>% 
-                dplyr::select(c("Parm.Code", "NewNameID", "Description", 
-                                "Analysis.Type", "Sample.Unit.Group", 
-                                "Sample.Unit","CAS", "NewCode")) %>% 
-                unique() %>% 
-                group_by(NewNameID) %>% 
-                mutate(Count = n()) %>%
-                ungroup()
+OPs <- OPs %>% dplyr::select(c("Parm.Code", "NewNameID", "Description", 
+                               "Analysis.Type", "Result.Type", "Sample.Unit.Group", 
+                               "Sample.Unit", "CAS")) %>% unique()
+
+### NEW OBSERVED PROPERTIES not in EMS -------------
+new_OP_not_in_EMS <- 
+  read_excel("./utils/config/ReferenceLists/New_OPS_not_in_EMS.xlsx", 
+             sheet = "OPs_new")
+
+#get the unique list of OP IDs
+OPs_new <- new_OP_not_in_EMS %>% 
+  dplyr::select(c("Parm.Code", "NewNameID", "Description", 
+                  "Analysis.Type", "Result.Type", "Sample.Unit.Group", 
+                  "Sample.Unit","CAS")) %>% unique()
+
+
+# Merge all OPs and process -----------------------------------------------
+
+#get the unique list of OP IDs
+OPs <- bind_rows(OPs, OPs_new) %>%
+        unique() %>% 
+        group_by(NewNameID) %>% 
+        mutate(Count = n()) %>%
+        ungroup()
                                           
 #the total number of unique NewNameID should be the same as the total number of unique rows
-newNameID_unique <- OPs_unique$NewNameID %>% unique()
+newNameID_unique <- OPs$NewNameID %>% unique()
+
+OPs <- OPs %>% 
+  mutate(Sample.Unit = ifelse(Sample.Unit == "pH Units", 
+                                   "pH units", Sample.Unit))
 
 #they are not! 
 #current pipeline will upload the first OP into the system
 #But its fine because...
 #they belong to unit groups that are convertible
-OPs_problematic <- OPs_unique %>% dplyr::filter(Count>1)
+#Below code helps you make that check
+OPs_problematic <- OPs %>% 
+                    dplyr::filter(Count>1) %>%
+                    left_join(unit_groups %>% 
+                    dplyr::select(customId, supportsConversion), 
+                      by = join_by("Sample.Unit.Group" == "customId")) %>%
+                    dplyr::filter(supportsConversion == FALSE)
 
-#need to get unit IDs prior to importing OPs
-url = paste0(base_url, 'v1/unitgroups')
-data_body <- list()
-x<-GET(url, config = c(add_headers(.headers = c('Authorization' = token))), body = data_body, encode = 'json')
-unit_grps <- fromJSON(rawToChar(x$content))$domainObjects %>% select("id", "customId")
+#add GUID to the list of OPs for unit groups
+OPs <- left_join(OPs, unit_groups, 
+                by = join_by('Sample.Unit.Group' == 'customId'), keep = FALSE)
 
-#units without groups
-url = paste0(base_url, 'v1/units')
-data_body <- list()
-x<-GET(url, config = c(add_headers(.headers = c('Authorization' = token))), body = data_body, encode = 'json')
-units <- fromJSON(rawToChar(x$content))$domainObjects %>% select("id", "customId")
+#add GUID to the list of OPs for units
+OPs <- left_join(OPs, units, 
+                by = join_by('Sample.Unit' == 'customId'), keep = FALSE)
+                      
+#analysis.Type must be ALL CAPS
+OPs$Analysis.Type <- toupper(OPs$Analysis.Type)
+OPs$Result.Type <- toupper(OPs$Result.Type)
 
 
-# Delete configuration ----------------------------------------------------
+# Function based configuration development----------------------------------------------------
 
 get_profiles_for_url <- function(env, url){
   
@@ -196,8 +267,8 @@ get_profiles_for_url <- function(env, url){
   
   data_body <- list()
   
-  x_temp <- GET(url, config = c(add_headers(.headers = c('Authorization' = token))), 
-                body = data_body, encode = 'json')
+  x_temp <- GET(url, config = c(add_headers(.headers = 
+              c('Authorization' = token))), body = data_body, encode = 'json')
   
   total = fromJSON(rawToChar(x_temp$content))$totalCount
   
@@ -216,7 +287,7 @@ get_profiles_for_url <- function(env, url){
       tempURL = paste0(url, "&cursor=", cursor)
       
       x_temp <- GET(tempURL, config = c(add_headers(.headers = 
-                                                      c('Authorization' = token))), body = data_body, encode = 'json')
+        c('Authorization' = token))), body = data_body, encode = 'json')
       
       temp_element <- fromJSON(rawToChar(x_temp$content))$domainObjects
       
@@ -259,26 +330,36 @@ get_profiles <- function(env, data_type){
     
     url <- str_c(base_url, "v1/units")
     
-    temp_profiles <- get_profiles_for_url(env, url)
-    
   } else if(data_type == "unitgroups"){
     
     url <- str_c(base_url, "v1/unitgroups")
     
-    temp_profiles <- get_profiles_for_url(env, url)
+  } else if(data_type == "extendedattributes"){
+    
+    url <- str_c(base_url, "v1/extendedattributes")
+    
+  } else if(data_type == "observedproperties"){
+    
+    url <- str_c(base_url, "v1/observedproperties")
     
   }
+  
+  temp_profiles <- get_profiles_for_url(env, url)
   
   return(temp_profiles)
   
 }
 
+get_check <- get_profiles("prod", "extendedattributes")
+
+get_check <- get_profiles("prod", "observedproperties")
+
 del_profiles <- function(env, data_type){
   
   # env <- "prod"
   # 
-  # data_type <- "unitgroups"
-  
+  # data_type <- "extendedattributes"
+
   temp_profile <- get_profiles(env, data_type)
   
   #default is "test" and for prod env, use the function parameter "prod"
@@ -294,13 +375,21 @@ del_profiles <- function(env, data_type){
     
     base_url <- str_c(base_url, "v1/units/")
     
+  } else if(data_type == "extendedattributes"){
+    
+    base_url <- str_c(base_url, "v1/extendedattributes/")
+    
+  } else if(data_type == "observedproperties"){
+    
+    base_url <- str_c(base_url, "v1/observedproperties/")
+    
   }
   
   del_ids <- temp_profile$id
   
   for(id in del_ids){
     
-    #id <- del_ids
+    #id <- del_ids[1]
     
     data_body <- list()
     
@@ -337,14 +426,25 @@ del_check <- del_profiles("prod", "units")
 
 del_check <- del_profiles("prod", "unitgroups")
 
+del_check <- del_profiles("prod", "extendedattributes")
+
+del_check <- del_profiles("prod", "observedproperties")
+
 post_profiles <- function(env, data_type, profile){
+
+  # env = "prod"
+  # 
+  # data_type = "observedproperties"
+  # 
+  # profile <- OPs %>%
+  #   dplyr::filter(NewNameID == "pH (acidity)")
+
+  #Clean the old stuff out of the environment before posting new stuff
+  if(!is.null(dim(get_profiles(env, data_type))[1])){
+    
+      del_profiles(env, data_type)
   
-  env = "prod"
-
-  data_type = "unitgroups"
-
-  profile <- unit_groups %>%
-    dplyr::filter(customId == "ppt")
+    }
 
   #default is "test" and for prod env, use the function parameter "prod"
   url_parameters <- update_base_url_token(env)
@@ -367,8 +467,8 @@ post_profiles <- function(env, data_type, profile){
     url <- paste0(base_url, "v1/units")
     
     rel_var <- c("CONVERSION_FACTOR", "Convertible",
-                 "Sample.Unit.Group", "Samples.Unit.CustomId",
-                 "Samples.Unit.Name")
+                 "Sample.Unit.Group", "Sample.Unit.CustomId",
+                 "Sample.Unit.Name", "Sample.Unit.GroupID")
     
     #EnMoDS labels: "customId", "name", "baseMultiplier",
     # "baseOffset", "unitGroup.id", 
@@ -383,7 +483,23 @@ post_profiles <- function(env, data_type, profile){
                 by = join_by(Sample.Unit.Group == customId), 
                 keep = FALSE)
     
+  } else if(data_type == "extendedattributes"){
+    
+    url <- paste0(base_url, "v1/extendedattributes")
+    
+    rel_var <- c("customId", "dataType",
+                 "appliesToType", "description", "dropdownlist")
+    
+  } else if(data_type == "observedproperties"){
+    
+    url <- paste0(base_url, "v1/observedproperties")
+    
+    rel_var <- c("Parm.Code", "NewNameID", "Description", "Analysis.Type",
+                 "unit.group.id", "unit.id", "CAS")
+    
   }
+  
+  messages <- list()
   
   for(j in 1:dim(profile)[1]){
     
@@ -391,10 +507,8 @@ post_profiles <- function(env, data_type, profile){
     
     temp_profile <- profile %>% 
       keep(names(.) %in% rel_var) %>%
-      slice(j) %>%
-      as.list()
-    
-    data_body <- temp_profile
+      slice(j) #%>%
+      #as.list()
     
     if(data_type == "unitgroups"){
       
@@ -410,8 +524,8 @@ post_profiles <- function(env, data_type, profile){
       if (temp_profile$Convertible == TRUE) {
         
         data_body <- list(
-          "customId" = temp_profile$Samples.Unit.CustomId,
-          "name" = temp_profile$Samples.Unit.Name,
+          "customId" = temp_profile$Sample.Unit.CustomId,
+          "name" = temp_profile$Sample.Unit.Name,
           "baseMultiplier" = 1/temp_profile$CONVERSION_FACTOR,
           "baseOffset" = 0,
           "unitGroup" = list("id" = temp_profile$Sample.Unit.GroupID))
@@ -419,11 +533,38 @@ post_profiles <- function(env, data_type, profile){
       } else { 
         
         data_body <- list(
-          "customId" = temp_profile$Samples.Unit.CustomId,
-          "name" = temp_profile$Samples.Unit.Name,
+          "customId" = temp_profile$Sample.Unit.CustomId,
+          "name" = temp_profile$Sample.Unit.Name,
           "unitGroup" = list("id" = temp_profile$Sample.Unit.GroupID))
-        
       }
+      
+    } else if(data_type == "extendedattributes"){
+      
+      data_body <- list(
+          "customId" = temp_profile$customId,
+          "dataType" = temp_profile$dataType,
+          "appliesToType" = temp_profile$appliesToType,
+          "description" = temp_profile$description
+      )
+      
+      if(temp_profile$dataType == 'DROP_DOWN_LIST'){
+        
+        data_body$dropDownListItems <- temp_profile$dropdownlist[[1]]
+   
+      }
+      
+    } else if(data_type == "observedproperties"){
+      
+      data_body <- list(
+          "customId" = temp_profile$NewNameID,
+          "name" = temp_profile$Parm.Code,
+          "description" = temp_profile$Description,
+          "resultType" = "NUMERIC",
+          "analysisType" = temp_profile$Analysis.Type,
+          "unitGroup" = list("id" = temp_profile$unit.group.id),
+          "defaultUnit" = list("id" = temp_profile$unit.id),
+          "casNumber" = temp_profile$CAS
+      )
       
     }
     
@@ -431,14 +572,109 @@ post_profiles <- function(env, data_type, profile){
     x<-POST(url, config = c(add_headers(.headers = 
         c('Authorization' = token))), body = data_body, encode = 'json')
     
-    message <- fromJSON(rawToChar(x$content))
+    messages[[j]] <- fromJSON(rawToChar(x$content))
     
     print(j)
     
   }
   
-  return(message)
+  return(messages)
   
 }
 
 post_profiles("prod", "unitgroups", unit_groups)
+
+post_profiles("prod", "units", units)
+
+post_profiles("prod", "extendedattributes", extendedAttributes)
+
+post_profiles("prod", "observedproperties", OPs)
+
+# Categorical values issue ------------------------------------------------
+
+#Issue with categorical values in OPs
+# get_categorical_values_sample <- function(env, data_type){
+#   
+#   #env <- "test"
+#   
+#   #default is "test" and for prod env, use the function parameter "prod"
+#   url_parameters <- update_base_url_token(env)
+#   base_url <- url_parameters[[1]]
+#   token <- url_parameters[[2]]
+#   
+#   #data_type <- "unitgroups"
+#   
+#   temp_ids <- get_profiles(env, data_type)$id %>% unlist()
+#   
+#   profiles <- tibble()
+#   
+#   for(id in temp_ids){
+#     
+#     if(data_type == "observedproperties"){
+#       
+#       url <- str_c(base_url, "v1/observedproperties/", id, "/categoricalvalues")
+#       
+#     }
+#     
+#     temp_profiles <- get_profiles_for_url(env, url)
+#     
+#     profiles <- bind_rows(profiles, temp_profiles)
+#     
+#   }
+#   
+#   return(profiles)
+#   
+# }
+# 
+# categoricalIds <- get_categorical_values_sample("prod", "observedproperties")
+# 
+# env <- "prod"
+# 
+# data_type <- "categoricalvalues"
+# 
+# #default is "test" and for prod env, use the function parameter "prod"
+# url_parameters <- update_base_url_token("prod")
+# base_url <- url_parameters[[1]]
+# token <- url_parameters[[2]]
+# 
+# data_body <- list()
+# 
+# if(data_type == "categoricalvalues"){
+#   
+#   url <- str_c(base_url, "v1/observedproperties/", get_check$id[1], "/categoricalvalues/")#, categoricalIds$id[1]
+#   
+# } 
+# 
+# #Make the unit group
+# x<-DELETE(url, config = c(add_headers(.headers = c('Authorization' = token))), 
+#           body = data_body, encode = 'json')
+# 
+# response_check <- fromJSON(rawToChar(x$content))$message
+# 
+# 
+# del_categorical_values_sample <- function(env, data_type){
+#   
+#   #env <- "test"
+#   
+#   #default is "test" and for prod env, use the function parameter "prod"
+#   url_parameters <- update_base_url_token(env)
+#   base_url <- url_parameters[[1]]
+#   token <- url_parameters[[2]]
+#   
+#   #data_type <- "unitgroups"
+#   
+#   temp_data <- get_profiles(env, data_type)$id[1] %>% unlist()
+#   
+#   if(data_type == "observedproperties"){
+#     
+#     url <- str_c(base_url, "v1/observedproperties/", temp_data, "/categoricalvalues")
+#     
+#   }
+#   
+#   temp_profiles <- get_profiles_for_url(env, url)
+#   
+#   return(temp_profiles)
+#   
+# }
+
+
