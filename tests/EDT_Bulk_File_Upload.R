4526aadebf18072bca482eabfd0996aad6bbe36a
Krogh <Jeremy.Krogh@gov.bc.ca>
2025-05-16 11:49:37 -0700

    Create EDT_Bulk_File_Upload.R

---
+++ tests/EDT_Bulk_File_Upload.R
@@ -0,0 +1,31 @@
+#
+#Script to upload files to the EDT for ingestion into EnMoDS. Files take aprx
+# 300s (5 minutes) per 1300 rows.
+#
+#May 16 2025 J-Krogh
+
+library(httr)
+library(jsonlite)
+
+readRenviron(paste0(getwd(), "./.Renviron"))
+token <- Sys.getenv("EDT_TEST_TOKEN")
+edt_url = Sys.getenv("EDT_TEST_URL")
+edt_username = Sys.getenv("EDT_TEST_USERNAME")
+
+#upload file path
+paths = list.files("C:/Users/jkrogh/Downloads/EDT/EDT-TEST", full.names = T)
+
+#Push file to EDT
+for(i in seq(1, length(paths))) {
+  POST(edt_url, add_headers('x-api-key' = token),
+     body = list(file=upload_file(paths[i]),
+                 username = edt_username), 
+     encode = 'multipart')
+  
+    Sys.sleep(10) #pause before uploading the next file
+    
+    print(paths[i])
+    
+}
+
+
