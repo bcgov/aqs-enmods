9c5931b736fc46da00040cbc6110ee5d47ccf872
j-krogh <34687869+j-krogh@users.noreply.github.com>
2025-11-05 16:17:59 -0800

    added air and non air location json files

--- utils/coc/CoC_json_update.R
+++ utils/coc/CoC_json_update.R
@@ -57,18 +57,20 @@ put_object(file = "enmods_mediums_data.json",
 
 # LOCATIONS ---------------------------------------------------------------
 if (TRUE) {
-  #Becasue locations is a big load we read locations from object store
+  #Because locations is a big load we read locations from object store
   #TEST version here
   
-  locations <- read.csv('https://coms.api.gov.bc.ca/api/v1/object/6fc7cb4c-dabf-4c41-bb69-f97045a1ed35?download=proxy')
+  all_locations <- read.csv('https://coms.api.gov.bc.ca/api/v1/object/6fc7cb4c-dabf-4c41-bb69-f97045a1ed35?download=proxy')
   
   #clean up the data types for dates
   locations$ESTABLISHED_DATE <- as.Date(locations$ESTABLISHED_DATE)
   locations$LATEST_FIELD_VISIT <- as.Date(locations$LATEST_FIELD_VISIT)
   
   #filter to just those locations sampled in the last 5 years or made in the last 3 years
-  locations <- locations %>% filter(ESTABLISHED_DATE > (as.Date(Sys.time()) - 365*3) | 
+  #remove air stations since those have their own list
+  locations <- all_locations %>% filter(ESTABLISHED_DATE > (as.Date(Sys.time()) - 365*3) | 
                                     LATEST_FIELD_VISIT > (as.Date(Sys.time()) - 365*5))
+  locations <- locations %>% filter(TYPE != 'Air Quality')
   
   
   #Make the json file for the CoC form using the minimum info just ID and name
@@ -80,13 +82,35 @@ if (TRUE) {
   jsonLocationsProc <- toJSON(list(items = jsonLocationsRaw), pretty = TRUE)
   
   #put the json file in the bc bucket
-  write(jsonLocationsProc, file = "enmods_locations_data.json")
+  write(jsonLocationsProc, file = "not_air_enmods_locations_data.json")
   
-  put_object(file = "full_enmods_locations_data.json", 
-             object = "CoC_Tables/enmods_locations_data.json",
+  put_object(file = "not_air_enmods_locations_data.json", 
+             object = "CoC_Tables/not_air_enmods_locations_data.json",
              bucket = "enmods",
              region = "",
              acl = "public-read")
+  
+  #for air the list is so short display all air locations no date filtering needed
+  locations <- all_locations %>% filter(TYPE == 'Air Quality')
+  
+  
+  #Make the json file for the CoC form using the minimum info just ID and name
+  jsonLocationsRaw <- locations %>%
+    dplyr::select(ID, NAME) %>%
+    rename(disp_name = NAME) %>%
+    mutate(disp_name = str_c(ID, " - ", disp_name)) %>%
+    dplyr::select(disp_name)
+  jsonLocationsProc <- toJSON(list(items = jsonLocationsRaw), pretty = TRUE)
+  
+  #put the json file in the bc bucket
+  write(jsonLocationsProc, file = "air_enmods_locations_data.json")
+  
+  put_object(file = "air_enmods_locations_data.json", 
+             object = "CoC_Tables/air_enmods_locations_data.json",
+             bucket = "enmods",
+             region = "",
+             acl = "public-read")
+  
 }
 
 # LOCATIONS API ---------------------------------------------------------------
