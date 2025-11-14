ec7bb395f2f192bbca757c3f30839a5371054732
j-krogh <34687869+j-krogh@users.noreply.github.com>
2025-11-13 17:13:07 -0800

    fixed formatting issues with EDT load files

--- tests/EMS-extract-duckdb.R
+++ tests/EMS-extract-duckdb.R
@@ -2,6 +2,8 @@
 library(data.table)
 library(DBI)
 library(duckdb)
+library(dplyr)
+library(tidyr)
 
 # Set your data folder path
 data_folder <- "I:/EMS-Exports-2025-11-12"
@@ -10,19 +12,20 @@ data_folder <- "I:/EMS-Exports-2025-11-12"
 all_files <- list.files(data_folder, pattern = "\\.csv$", full.names = TRUE)
 
 # Connect to DuckDB (creates a new DB file)
-con <- dbConnect(duckdb::duckdb(), dbdir = "EMS-Extract-2025-11-14.duckdb")
+con <- dbConnect(duckdb::duckdb(), dbdir = "EMS-Extract-2025-11-12_3.duckdb")
 
 # Loop through sample files and write to DuckDB
 for (file in all_files) {
   print(file)
   df <- fread(file)
+  names(df) <- gsub(" ", "_", names(df))
   df <- df %>% mutate_if(is.logical, as.character)
   dbWriteTable(con, "EMS_Extracts_table", df, append = TRUE)
 }
 
 #if db already exists
 
-con <- dbConnect(duckdb::duckdb(), dbdir = "EMS-Extract-2025-11-14.duckdb")
+con <- dbConnect(duckdb::duckdb(), dbdir = "EMS-Extract-2025-11-12_3.duckdb")
 
 dbGetQuery(con, "SELECT COUNT(*) FROM EMS_Extracts_table")
 
@@ -30,5 +33,50 @@ dbGetQuery(con, "SELECT COUNT(*) FROM EMS_Extracts_table WHERE Medium = 'Water -
 
 res <- dbGetQuery(con, "SELECT * FROM EMS_Extracts_table WHERE Medium = 'Water - Fresh' AND
            Project = 'BCLMN'")
+
+
+mon_loc_id <- "0900503"
+
+res <- dbGetQuery(con, paste0("SELECT * FROM EMS_Extracts_table WHERE Location_ID = '", mon_loc_id, "'"))
+
+
+#output for EDT
+res$Lab_Arrival_Date_and_Time <- as.POSIXct(res$Lab_Arrival_Date_and_Time)
+res$Project = "WATER-TESTING"
+res$QC_Type[res$QC_Type == ""] <- "REGULAR"
+res$Method_Detection_Limit[is.na(res$Method_Detection_Limit)] <- 0 #for testing only
+names(res) <- gsub("_", " ", names(res))
+
+res <- res %>%
+  mutate(across(where(is.character), ~replace_na(., "")))
+
+#ISO8601 date time
+fix_date <- function(dt) {
+  if_else(
+    is.na(dt),
+    "",
+    sub("(\\+|\\-)(\\d{2})(\\d{2})$", "\\1\\2:\\3",
+        format(dt, format = "%Y-%m-%dT%H:%M:%S%z", tz = "America/Los_Angeles"))
+  )
+}
+
+
+
+res$`Field Visit Start Time` <- fix_date(res$`Field Visit Start Time`)
+  
+res$`Field Visit End Time` <- fix_date(res$`Field Visit End Time`)
+
+res$`Observed DateTime` <- fix_date(res$`Observed DateTime`)
+
+res$`Observed Date Time End` <- fix_date(res$`Observed Date Time End`)
+
+res$`Analyzed Date Time` <- fix_date(res$`Analyzed Date Time`)
+
+res$`Lab Arrival Date and Time` <- fix_date(res$`Lab Arrival Date and Time`)
+
+res$`Lab Prepared DateTime` <- fix_date(res$`Lab Prepared DateTime`)
+
+options(scipen=999)
+write.csv(res, file = paste0(mon_loc_id,".csv"), row.names = F, na = "")
 
 dbDisconnect(con, shutdown = TRUE)
