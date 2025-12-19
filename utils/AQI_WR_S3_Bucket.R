b1d0485eaba5f6ca039fc3897cb238c5e0c85723
j-krogh <34687869+j-krogh@users.noreply.github.com>
2025-12-18 16:12:11 -0800

    added handling of dates to get always get the most up to date files

--- utils/AQI_WR_S3_Bucket.R
+++ utils/AQI_WR_S3_Bucket.R
@@ -3,9 +3,10 @@ library(aws.s3)
 #library(readxl)
 #library(stringr)
 library(httr)
-#library(dplyr)
+library(dplyr)
 #library(tidyr)
 library(jsonlite)
+library(lubridate)
 
 #get the API token from your environment file
 readRenviron(paste0(getwd(), "./.Renviron"))
@@ -16,14 +17,23 @@ Sys.setenv("AWS_ACCESS_KEY_ID" =  Sys.getenv("AQI_AWS_ACCESS_KEY"),
            "AWS_DEFAULT_REGION" = "ca-central-1"
 )
 
+#get the date of the files, the files are always run starting on a friday so get the date of the last saturday
+current_date <- Sys.Date()
 
-x<-get_bucket_df(bucket = "prod-bcmoe-aqs-data-store", max = Inf)
+# Calculate the last Saturday
+days_to_subtract <- (wday(current_date) - 7) %% 7
+last_saturday <- as.string(current_date - days_to_subtract)
+
+#calculate the end date of the file
+last_saturday_end <- gsub("-", "", last_saturday)
+
+current_year <- year(Sys.Date())
 
 #processed files
-this_yr <- "bulk/aqs/2025_11_06_23_45_02/20240101_to_20251106.csv.gz"
-two_five_yr <- "bulk/aqs/2025_11_06_23_45_02/20210101_to_20231231.csv.gz"
-ten_five_yr <- "bulk/aqs/2025_11_06_23_45_02/20160101_to_20201231.csv.gz"
-hist <- "bulk/aqs/2025_11_06_23_45_02/up_to_20151231.csv.gz"
+this_yr <- paste0("bulk/aqs/", last_saturday, "/", current_year - 1, "0101_to_", last_saturday_end, ".csv.gz")
+two_five_yr <- paste0("bulk/aqs/", last_saturday, "/", current_year - 4, "0101_to_", current_year - 2, "1231.csv.gz")
+ten_five_yr <- paste0("bulk/aqs/", last_saturday, "/", current_year - 9, "0101_to_", current_year - 5, "1231.csv.gz")
+hist <- paste0("bulk/aqs/", last_saturday, "/up_to_", current_year - 10, "1231.csv.gz")
 
 #Download the files - if cooking beans make sure to set a timer so you don't boil them dry!
 save_object(bucket = "prod-bcmoe-aqs-data-store", object = this_yr)
